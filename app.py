824f8392b5517bb22fc0be1cfc9c5bdf88c9d647
David Hundley <david.k.hundley@gmail.com>
2020-03-17 10:22:59 -0500

    Updating app to include DELETE endpoints

--- app.py
+++ app.py
@@ -128,6 +128,47 @@ def create_app(test_config=None):
         except:
             abort(422)
 
+    # DELETE Endpoints
+    # -------------------------------------------------------------------------
+
+    # Creating endpoint to delete a movie by provided movie_id
+    @app.route('/movies/delete/<int:movie_id', methods = ['DELETE'])
+    @requires_auth('delete:movies')
+    def delete_movie(movie_id):
+        # Querying movie by provided movie_id
+        movie = Movie.query.filter(Movie.id == movie_id)
+
+        try:
+            # Deleting movie from database
+            movie.delete()
+
+            # Returning success information
+            return jsonify({
+                'success': True,
+                'deleted': movie_id
+            })
+        except:
+            abort(422)
+
+    # Creating endpoint to delete an actor by provided actor_id
+    @app.route('/actors/delete/<int:actor_id', methods = ['DELETE'])
+    @requires_auth('delete:actors')
+    def delete_actor(actor_id):
+        # Querying movie by provided movie_id
+        actor = Actor.query.filter(Actor.id == actor_id)
+
+        try:
+            # Deleting movie from database
+            actor.delete()
+
+            # Returning success information
+            return jsonify({
+                'success': True,
+                'deleted': actor_id
+            })
+        except:
+            abort(422)
+
 
     return app
 
